9e2e879a1e4b62b983b5fa141d926caf14a8f6db
RishiTandon <findrishi@gmail.com>
2019-10-23 19:37:55 +0530

    Added p4 scripts
    Signed-off-by: RishiTandon <findrishi@gmail.com>

---
+++ p4rt-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#FIXME update cfg directory to path based on location of this shell script
+
+docker run -it --rm \
+  -v $PWD/cfg:/tmp/cfg \
+  p4lang/p4runtime-sh \
+  --grpc-addr host.docker.internal:50001 \
+  --device-id 1 --election-id 0,1 \
+  --config /tmp/cfg/basic_p4info.txt,/tmp/cfg/basic.json
